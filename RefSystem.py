8f96b616101b713ecf11f3b158e2a92adea550df
John Hellborg <john_._93@hotmail.com>
2020-03-23 19:24:33 +0100

    Added grid

--- RefSystem.py
+++ RefSystem.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import csv
 
 class RefSystem(object):
 
@@ -6,6 +7,30 @@ class RefSystem(object):
         self.root = tk.Tk()
         self.root.title('Referee system')
         self.root.geometry('700x400')
+        self.data = []
     
     def run(self):
         tk.mainloop()
+
+    def initFromCSV(self, filename):
+        """
+        Column 1: Day
+        Column 2: Month
+        Column 3: Club
+        Column 4: Travel money
+        Column 5: Salary
+        """
+        with open(filename) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=";")
+
+            for row in csv_reader:
+                self.data.append(row)
+            
+
+    def initGrid(self):
+        
+        for i in range(len(self.data)):
+            for j in range(len(self.data[1])):
+                e = tk.Entry(self.root)
+                e.insert(tk.END, self.data[i][j])
+                e.grid(row = i, column = j)
--- Test.py
+++ Test.py
@@ -1,6 +1,8 @@
 from RefSystem import *
 
 rs = RefSystem()
+rs.initFromCSV("Example.txt")
+rs.initGrid()
 rs.run()
 
 print("Hello world")
